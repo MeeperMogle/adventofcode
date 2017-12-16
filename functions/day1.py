fb7f960cd355685364cdb091fae336b46ffbd4e6
Jonas Olsson <jonas.olsson1993@gmail.com>
2017-12-16 09:20:34 +0100

    Solve whole day 1

--- functions/day1.py
+++ functions/day1.py
@@ -11,3 +11,16 @@ def solve_captcha_day1_1(arr):
         captcha_sum += arr[i] if arr[i] == arr[i + 1] else 0
 
     return captcha_sum
+
+
+def solve_captcha_shift(arr, shift=1):
+    # Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular
+    # list. That is, if your list contains 10 items, only include a digit in your sum if the digit
+    # 10 / 2 = 5 steps forward matches it.Fortunately, your list has an even number of elements.
+    captcha_sum = 0
+
+    for i in range(len(arr) - 1):
+        compare_index = i + shift if i + shift < len(arr) else i + shift - len(arr)
+        captcha_sum += arr[i] if arr[i] == arr[compare_index] else 0
+
+    return captcha_sum
--- tests/day1.py
+++ tests/day1.py
@@ -1,10 +1,11 @@
 import unittest
-from functions.day1 import solve_captcha_day1_1
+from functions.day1 import solve_captcha_day1_1, solve_captcha_shift
 
 
 class MyTestCase(unittest.TestCase):
     def test_first(self):
-        # 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
+        # 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2)
+        #   matches the fourth digit.
         # 1111 produces 4 because each digit (all 1) matches the next.
         # 1234 produces 0 because no digit matches the next.
         # 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
@@ -18,6 +19,44 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(solve_captcha_day1_1(example2), 4)
         self.assertEqual(solve_captcha_day1_1(example3), 0)
         self.assertEqual(solve_captcha_day1_1(example4), 9)
+
+    def test_full(self):
+        # 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2)
+        #   matches the fourth digit.
+        # 1111 produces 4 because each digit (all 1) matches the next.
+        # 1234 produces 0 because no digit matches the next.
+        # 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+        example1 = [1, 1, 2, 2]
+        example2 = [1, 1, 1, 1]
+        example3 = [1, 2, 3, 4]
+        example4 = [9, 1, 2, 1, 2, 1, 2, 9]
+
+        self.assertEqual(solve_captcha_shift(example1), 3)
+        self.assertEqual(solve_captcha_shift(example2), 4)
+        self.assertEqual(solve_captcha_shift(example3), 0)
+        self.assertEqual(solve_captcha_shift(example4), 9)
+
+        self.assertEqual(solve_captcha_shift(example1), solve_captcha_day1_1(example1))
+        self.assertEqual(solve_captcha_shift(example2), solve_captcha_day1_1(example2))
+        self.assertEqual(solve_captcha_shift(example3), solve_captcha_day1_1(example3))
+        self.assertEqual(solve_captcha_shift(example4), solve_captcha_day1_1(example4))
+
+        # 1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+        # 1221 produces 0, because every comparison is between a 1 and a 2.
+        # 123425 produces 4, because both 2s match each other, but no other digit has a match.
+        # 123123 produces 12.
+        # 12131415 produces 4.
+        example1 = [1, 2, 1, 2]
+        example2 = [1, 2, 2, 1]
+        example3 = [1, 2, 3, 4, 2, 5]
+        example4 = [1, 2, 3, 1, 2, 3]
+        example5 = [1, 2, 1, 3, 1, 4, 1, 5]
+
+        self.assertEqual(solve_captcha_shift(example1, int(len(example1) / 2)), 6)
+        self.assertEqual(solve_captcha_shift(example2, int(len(example2) / 2)), 0)
+        self.assertEqual(solve_captcha_shift(example3, int(len(example3) / 2)), 4)
+        self.assertEqual(solve_captcha_shift(example4, int(len(example4) / 2)), 12)
+        self.assertEqual(solve_captcha_shift(example5, int(len(example5) / 2)), 4)
 
 
 if __name__ == '__main__':
